34556bf497ee946459f5b7e10b7be5083ac2ffb2
chahyeonje <guswp3611@gmail.com>
2022-07-05 16:55:04 +0900

    editted pathOS.py

--- camel-code-raisim/pathOS.py
+++ camel-code-raisim/pathOS.py
@@ -1,2 +1,2 @@
-#pathos='/home/cha/raisimLib-21842290d961ecf40e702f108494720b7e5df7f6/build/lib'
-pathos='C:/Users/camelcha/raisimLib/install/bin'
+pathos='/home/cha/raisimLib/install/lib'
+#pathos='C:/Users/camelcha/raisimLib/install/bin'
