efd70e839eb2a6cfccc3f7db30f281a8b1c0bb26
chahyeonje <guswp3611@gmail.com>
2022-07-26 14:50:33 +0900

    optimized gps code

--- gps/Gps.py
+++ gps/Gps.py
@@ -6,6 +6,7 @@ class Gps:
     def __init__(self):
         self.port = '/dev/ttyUSB0'
         self.ser = serial.Serial(self.port, baudrate=9600)
+        self.firstRun = True
         
     def recordOutput(self,title):
         f = open(title,'a')
@@ -25,13 +26,33 @@ class Gps:
         else :
             pass
 
-        
+    def setupGps(self, goallat, goallng):
+        lat, lng = self.getGps()
+        self.startGpsPosition(lat, lng)
+        self.goalGpsPosition(goallat, goallng)
 
     def startGpsPosition(self, latitude, longitude):
         self.startLat, self.startLng = latitude,longitude 
 
-    def currentGpsPosition(self, latitude, longitude):
-        self.currentLat, self.currentLng = latitude, longitude
+    def currentGpsPosition(self):
+        self.latIn, self.lngIn = self.getGps()
+        if(self.firstRun == True):
+            prevLat = self.startLat
+            prevLng = self.startLng
+            self.firstRun = False
+        else : 
+            pass
+        
+        if(self.latIn == 0):
+            self.currentLat = prevLat
+            self.currentLng = prevLng
+        else:
+            self.currentLat = self.latIn
+            self.currentLng = self.lngIn
+            prevLat = self.latIn
+            prevLng = self.lngIn
+        
+        return self.currentLat, self.currentLng
 
     def goalGpsPosition(self, latitude, longitude):
         self.goalLat, self.goalLng = latitude, longitude
@@ -41,15 +62,14 @@ class Gps:
         self.distanceToEast = (self.goalLng - self.startLng)*100000*0.98
         return self.distanceToNorth, self.distanceToEast
 
-    def getDistanceCurrentToGoal(self):
-        self.distanceToNorth = (self.goalLat - self.currentLat)*100000*1.08
-        self.distanceToEast = (self.goalLng - self.currentLng)*100000*0.98
+    def getDistanceCurrentToGoal(self): #현재 위치에서 목적지까지 북,동 방향으로 각각 몇 미터 남았는지 알려줌
+        self.flat, self.flng = self.currentGpsPosition()
+        self.distanceToNorth = (self.goalLat - self.flat)*100000*1.08 #단위 : m
+        self.distanceToEast = (self.goalLng - self.flng)*100000*0.98 #단위 : m
         return self.distanceToNorth, self.distanceToEast
 
     def showState(self):
-        lat, lng = self.getGps()
-        self.currentGpsPosition(lat,lng)
         distanceN, distanceE = self.getDistanceCurrentToGoal()
         print("북쪽으로 남은 거리 : ", distanceN, "동쪽으로 남은 거리 : ", distanceE)
-        print("현재 위치: ",lat,lng)
+        print("현재 위치: ",self.flat,self.flng)
         print(" ")
--- gps/testGps.py
+++ gps/testGps.py
@@ -1,17 +1,17 @@
 import Gps
 
 testGps = Gps.Gps()
-startlat,startlng = testGps.getGps()
-testGps.startGpsPosition(startlat, startlng)
-print("----startposition. lat: ", startlat," lng: ", startlng ,"----")
-testGps.goalGpsPosition(35.2323, 129.0824)
+
+testGps.setupGps(35.2323,129.0824)
+
 distanceN, distanceE = testGps.getDistanceStartToGoal()
+print("----startposition. lat: ", testGps.startLat," lng: ", testGps.startLng ,"----")
 print("북쪽으로 남은 거리 : ", distanceN, "동쪽으로 남은 거리 : ", distanceE)
 
 while True:
     try:
         testGps.showState()
-        testGps.recordOutput('0725')
+        #testGps.recordOutput('0725')
     except:
         continue
 
